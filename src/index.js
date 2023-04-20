c39ccf912042a446d79081d72409da6f3c4e878a
Olivia Webster <oliviacweb@gmail.com>
2023-04-19 20:53:25 -0600

    add toast container

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+import './index.css'
+import 'react-toastify/dist/ReactToastify.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ToastContainer />
       <App />
     </Provider>
   </React.StrictMode>
