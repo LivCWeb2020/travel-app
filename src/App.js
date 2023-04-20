99474b228cd8171f6fb0e3e2d769c7ca0336aa73
Olivia Webster <oliviacweb@gmail.com>
2023-04-19 19:32:57 -0600

    create inital app container and material UI app bar and toolbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Container from './components/Container';
 
 export default function App () {
   return (
     //layout will wrap cities
-    <>Hello World</>
+    <Container />
   )
 }
---
+++ src/components/Container.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+  ThemeProvider,
+  CssBaseline,
+  createTheme,
+  FormControlLabel,
+  Switch
+} from '@mui/material'
+import { AppBar, Toolbar } from '@mui/material'
+
+const Container = ({ children }) => {
+
+  return (
+    <>
+      <CssBaseline />
+      <AppBar
+        color='primary'
+        sx={{
+          position: 'fixed',
+          width: '100%'
+        }}
+      >
+        <Toolbar
+          sx={{
+            fontWeight: 'bold',
+            fontFamily: 'Open Sans',
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}
+        >
+          <h3>Cities of the World</h3>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default Container
