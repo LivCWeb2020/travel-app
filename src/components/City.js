2b3ae0559a13c47b46303b8827c08a03a4364987
Olivia Webster <oliviacweb@gmail.com>
2023-04-19 22:14:14 -0600

    create placeholder update city function

--- src/components/City.js
+++ src/components/City.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import {
   Card,
+  CardMedia,
   CardContent,
   Checkbox,
   FormControlLabel
 } from '@mui/material'
 
-
-export default function City () {
-  // Update city visited status on Firebase function here
- 
+export default function City ({ city, cities }) {
+  // Update city visited status on Firebase
+  const handleUpdateCity = async () => {
+    console.log('update city')
+  }
 
   return (
     <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component='img'
+        height='194'
+      />
       <CardContent
         sx={{
           display: 'flex',
@@ -22,13 +28,14 @@ export default function City () {
         }}
       >
         <div>
-          <h3 className='text-xl font-bold  '>City</h3>
-          <p className='text-sm '>Country</p>
+          <h3 className='text-xl font-bold  '>city</h3>
+          <p className='text-sm '>country</p>
         </div>
         <FormControlLabel
           labelPlacement='end'
           control={
             <Checkbox
+              onChange={() => handleUpdateCity()}
               inputProps={{ 'aria-label': 'visited' }}
             />
           }
